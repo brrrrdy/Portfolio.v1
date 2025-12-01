198044925d997a466e23bfb7c43e4e0d3246cb27
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-12-01 16:35:15 +0100

    color changer exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./index.css";
+import Colors from "./components/Colors";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-blue-900 text-white">
       <div id="top" className="h-px w-full relative" />
+
+      <Colors />
 
       <div className="flex-1 flex items-center justify-center">
         <h1 className="text-4xl font-bold text-white">Portfolio Coming Soon</h1>
